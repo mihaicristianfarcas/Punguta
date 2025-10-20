f468cecfdc12cfd96541ef5638646100b80d9e9c
Mihai-Cristian Farcaș <146859576+mihaicristianfarcas@users.noreply.github.com>
2025-10-20 08:36:28 +0300

    Initial Commit

---
+++ Punguta/PungutaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PungutaApp.swift
+//  Punguta
+//
+//  Created by Mihai-Cristian Farcas on 20.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct PungutaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
